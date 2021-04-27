8549e0cd8003ca8f62d5297cd4f94c811c334559
Ben <S00200287@gmail.com>
2021-04-27 13:23:38 +0100

    Re did a few things cause it stopped working went and grabbed my older files to copy in

--- index.js
+++ index.js
@@ -1,54 +1,121 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const testData = require('./lib/data.js');
+const port = process.env.PORT || 3000;
 
 
-// Import Routes
+// the database
+
+require('./lib/db');
+// import all the routers
+
 const baseRouter = require('./routes/base');
 const staffRouter = require('./routes/staff');
 
-// Cookies 
-const cookieParser = require('cookie-parser');
-app.use(cookieParser("Ben is a God!!"));
 
-// need this before you can use req.body
-app.use(express.urlencoded({ extended: true })) 
+// set up cookie handling middleware
+
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser("Ben is god!!"));
+
+// middleware for parsing the body of a form need this before you can use req.body
+
+app.use(express.urlencoded({ extended: true }))
+
+// set up session handling middleware: note this uses cookies so that needs to be set
+// up too.
 
 session = require('express-session');
 
-app.use(session(
-    {secret: "una is great!!", 
-    cookie: { maxage: 6000},
-    resave: false,
-    saveUninitialized: false
-  }))
 
+app.use(session(
+    {
+        secret: "Ben is god !!",
+        cookie: { maxage: 6000 },
+        resave: false,
+        saveUninitialized: false
+    }))
 
 
 // set up handlebars view engine
-var handlebars = require('express-handlebars')
-.create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        urlencodetext: function (text){
+            return encodeURI(text);
+        }
+    }
+    });
+    //     eachinputfield: function (context, options) {
+    //         var returnString = "";
+
+    //         if (context){
+          
+
+    //         for (var i = 0; i < context.length; i++) {
+    //             hobbyString = "hobby" + i;               
+    //             inputElementString = "<input class='form-control' type='text'  " + " name=" +
+    //             hobbyString  + " id=" + hobbyString + " value=" + context[i] + " > " 
+
+    //             returnString += "<div class='form-group' > <label > Hobby " + (i+1) + " </label>" + 
+    //             inputElementString + " </div>"
+    //         }
+
+    //         return returnString;
+    //     }}
+    // }
+    // });
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+
+// helpers: { eachInputField:  function(context, options) {
+//     var reurnString = "";
+
+//     for(var i =0 ; i < context.length; i++)
+//     {
+//         hobbyString = "hobby"+i;
+//         returnString  += "<input type='text' value= options.fn(context[i]) name=hobbyString id=hobbyString>"
+//     }
+
+//     return returnString; }}});
+
+
+
+// static files can be served from the public folder
+
 app.use(express.static('public'));
 
-app.use('/',baseRouter);
-app.use('/staff',staffRouter);
 
-app.get('/', function (req, res) {
-    res.render('home');
+// import our own Middleware
+
+const { flashMiddleware, newsMiddleware, testMiddleware } = require('./lib/middleware.js');
+app.use(flashMiddleware);
+//app.use(testMiddleware);
+
+
+// settting up our own routes
+
+app.use('/', baseRouter);
+
+app.use('/staff', staffRouter)
+
+
+
+
+
+
+
+
+
+// custom 404 page
+app.use((req, res) => {
+
+    res.render('404');
 });
 
-// Custom 404 page
-app.use( (req, res) => {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
-});
-
-// Custom 500 page
+// custom 500 page
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.type('text/plain');
@@ -57,6 +124,4 @@ app.use((err, req, res, next) => {
 });
 
 
-
-app.get('/', (req, res) => res.send('Hello World from Ben!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- routes/staff.js
+++ routes/staff.js
@@ -3,47 +3,22 @@ const router = express.Router();
 
 // import the data we need
 
-const testData = require('../lib/data.js');
+const testData = require('../lib/db.js');
 
-console.table(testData.getPeopleData());
+router.get('/', async (req, res) => {
+    const data = await testData.readStaff();
+
+    res.render('personlist', { personlist: data })
+});
+
 
 
 
 router.get('/addnew', (req, res) => {
-    let fname = req.query.firstname;
-    let sname = req.query.surname;
-    console.log('Date entered ' + fname + ' ' + sname);
-    let data = {}
 
-    res.render('personform', {firstname: fname, surname: sname})
+    res.render('personform')
 }
 )
-router.get('/addnew', (req, res) =>
-res.render('personform')
-)
-router.get('/personadded', (req, res) =>
-res.render('personadded'))
-
-router.post('/addnew', (req, res) => {
-console.log("Data send via post");
-console.table(req.body);
-res.redirect(303, 'personadded',)
-})
-
-router.get('/:name',  (req, res) => {
-
-    var name = req.params.name;
-    var data = testData.getPeopleData();
-
-    if (data[name] == null) {
-        res.render('404'); // could also have a special page for person not found
-    }
-    else {
-        res.render('person', { person: data[name] })
-    }
-
-
-})
 
 router.get('/personadded', (req, res) => {
 
@@ -55,18 +30,111 @@ router.get('/personadded', (req, res) => {
     }
     res.render('personadded', { name: newName })
 })
+
+// router.post('/addnew', (req, res) => {
+//      let fname = req.body.firstname;
+//      let sname = req.body.surname;
+//      console.log('Data entered ' + fname + ' ' + sname);   
+//      res.render('personform', {name1 : fname, name2: sname})
+
+// })
+
+// This implments POST/REDIRECT/GET it uses
+// a session to take data from this page to the personadded page.
+
+// router.post('/addnew', (req, res) => {
+//     console.log("Data received froma  post");
+//     console.table(req.body);
+//     req.session.staffdata = { name: req.body.firstname + " " + req.body.surname }
+//     res.redirect(303, '/staff/personadded',)
+// })
+
+// This implements POST/REDIRECT/GET
+//this time is uses a flash message
+
 router.post('/addnew', (req, res) => {
     console.log("Data received from a  post");
     console.table(req.body);
-    req.session.flash = 
-    { type: 'success', intro: 'Data Saved:', message:  "Data for <strong>" + req.body.firstname + " " + req.body.surname + "</strong> has been added"}
-    res.redirect(303, '/staff')
+
+    // note: this is not safe code. Any inputs from a user should be validated and sanitised before
+    // being saved to the database.
+
+
+    testData.createStaff(req.body)
+        .then(() => {
+            req.session.flash =
+                { type: 'success', intro: 'Data Saved:', message: "Data for <strong>" + req.body.name + "</strong> has been added" }
+            res.redirect(303, '/staff')
+        }
+        )
+        .catch(() => {
+            req.session.flash =
+                { type: 'danger', intro: 'Data not saved:', message: "Data for <strong>" + req.body.name + "</strong> has not been added" }
+            res.redirect(303, '/staff')
+        }
+        )
+    })
+       
+
+
+
+
+
+router.get('/:name', async (req, res) => {
+
+    var name = req.params.name;
+    var data = await testData.readStaff({ name: name });
+
+    // data is an array which contains all the staff whose name matches.
+    // there should only be one and we will take the
+
+    if (!data[0]) {
+        res.render('404'); // could also have a special page for person not found
+    }
+    else {
+        res.render('person', { person: data[0] })
+    }
 })
-router.post('/addnew', (req, res) => {
-    console.log("Data received from a  post");
+
+router.get('/:name/edit', async (req, res) => {
+    var name = req.params.name;
+    var data = await testData.readStaff({ name: name });
+
+    res.render('personeditform', { person: data[0] })
+})
+
+router.get('/:name/delete', async (req, res) => {
+    var name = req.params.name;
+
+    await testData.deleteStaff(name)
+        .then(() => {
+            req.session.flash =
+                { type: 'success', intro: 'Data Removed:', message: "<strong>" + name + "</strong> has been removed" }
+            res.redirect('/staff')
+        })
+        .catch(() => {
+            req.session.flash =
+                { type: 'danger', intro: 'Data not Removed:', message: "<strong>" + name + "</strong> has not been removed" }
+            res.redirect('/staff')
+        });
+});
+
+
+
+router.post('/:name/edit', async (req, res) => {
+    console.log("Data received from a Edit post");
     console.table(req.body);
-    req.session.staffdata = { name: req.body.firstname + " " + req.body.surname };
-    res.redirect(303, '/staff/personadded')
+    testData.updateStaff(req.body)
+        .then(() => {
+            req.session.flash =
+                { type: 'success', intro: 'Data Edited:', message: "Data for <strong>" + req.body.name + "</strong> has been edited" }
+            res.redirect(303, '/staff')
+        })
+        .catch(() => {
+            req.session.flash =
+                { type: 'danger', intro: 'Data not Edited:', message: "Editing failed" }
+            res.redirect('/staff')
+        });
 })
 
 module.exports = router;
